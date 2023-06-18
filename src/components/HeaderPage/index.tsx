0fcc03939b50a130d432bb162156e89228b81bfe
felipe-AGomes <falmeidagomes13@gmail.com>
2023-06-18 10:40:44 -0300

    feat: add style switch on cart icon in the header

--- src/components/HeaderPage/index.tsx
+++ src/components/HeaderPage/index.tsx
@@ -3,8 +3,11 @@ import { HiOutlineSearch } from 'react-icons/hi';
 import S from './HeaderPage.module.css';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import CartNumber from '../CartNumber';
+import useAppContext from '@/hooks/useAppContext';
 
 export default function HeaderPage() {
+	const { cart, setCart } = useAppContext();
+
 	return (
 		<header className={S.header__contain}>
 			<form className={S.form__contain}>
@@ -16,7 +19,10 @@ export default function HeaderPage() {
 					<HiOutlineSearch size={28} />
 				</button>
 			</form>
-			<div className={`${S.cartIcon__contain}`}>
+			<div
+				className={`${S.cartIcon__contain} ${cart ? S.active : ''}`}
+				onClick={() => setCart(!cart)}
+			>
 				<AiOutlineShoppingCart size={28} />
 				<CartNumber
 					style={{
